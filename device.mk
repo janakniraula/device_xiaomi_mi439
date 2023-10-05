fc2853621cb1b121d315fdfb934bf70f39fc6a04
me-cafebabe <me.cafebabe@gmail.com>
2023-10-06 04:32:27 +0800

    mi439: libstdc++.vendor => libstdc++_vendor
    Change-Id: I478eb89aa2ac106d855b168844863032612bc8e8

--- device.mk
+++ device.mk
@@ -80,7 +80,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0.vendor \
-    libstdc++.vendor \
+    libstdc++_vendor \
     camera.msm8937
 
 ifeq ($(USE_GRAPHENEOS_CAM), true)
