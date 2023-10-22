07b1f09603e8a0971e42386c4d584d83abc983b4
Jprimero15 <jprimero15@aospa.co>
2023-10-23 05:05:26 +0800

    mi439: configs: hidl: Merge device manifests
    Change-Id: I0e8f94152b3ff6774daf462f1781eea731f76597
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,8 +99,7 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest_k4.9.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_sdm439
